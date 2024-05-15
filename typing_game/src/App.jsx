bd6cc1666bd8df9cde23b18f348c3d8f40a965b5
donguri7 <i.takumi1317@gmail.com>
2024-05-15 18:12:56 +0900

    app configuration

---
+++ typing_game/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from './logo.svg';
+import Helloworld from "./trial/helloworld";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Helloworld />
+    </div>
+  );
+}
+
+export default App;
